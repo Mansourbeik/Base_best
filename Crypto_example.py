3513c29a4a97befb8720c788f06e412ab6791dfc
Mansour <129630685+Mansourbeik@users.noreply.github.com>
2026-06-28 18:02:57 +0330

    Create Crypto_example.py
    This is my first commit for this project

---
+++ Crypto_example.py
@@ -0,0 +1,16 @@
+{
+  "name": "my-project",
+  "version": "1.0.0",
+  "main": "src/index.js",
+  "scripts": {
+    "start": "node src/index.js"
+  },
+  "license": "MIT"
+}
+console.log("Hello GitHub 🚀");
+
+function greet(name) {
+  return `Hello ${name}, project is running!`;
+}
+
+console.log(greet("Mansour"));
